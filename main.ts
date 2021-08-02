69c4ef562ce27d018717511fb848ebf20b7edc03
kawarimidoll <kawarimidoll+git@gmail.com>
2021-08-02 18:45:12 +0900

    chore(init): remove scaffolded files

--- main.ts
+++
@@ -1,6 +0,0 @@
-import { Logger } from "./logger.ts";
-
-Logger.debug("This log is debug!");
-Logger.info("This log is info!");
-Logger.warning("This log is warning!");
-Logger.error("This log is error!");
