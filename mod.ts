83ba01beb64033f5e5101850f6fe8fd829973ae1
kawarimidoll <kawarimidoll+git@gmail.com>
2021-08-03 10:00:18 +0900

    feat: add cli.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,4 @@
-import { parseYaml, serve, tag as h } from "./deps.ts";
-import { createDeployServer } from "./create_deploy_server.ts";
+import { parseYaml, tag as h } from "./deps.ts";
 import { getDenoteCss } from "./css_functions.ts";
 
 import {
@@ -8,29 +7,41 @@ import {
   ProfileConfiguration,
 } from "./types.ts";
 
-const {
-  name,
-  projectName,
-  title: titleInConfig,
-  bio,
-  // TODO: use disableFlags
-  // disable,
-  avatar,
-  favicon,
-  twitter: twitterInConfig,
-  list: listInConfig,
-} = parseYaml(Deno.readTextFileSync("./denote.yml")) as ProfileConfiguration;
+export function loadConfig(filename: string): ProfileConfiguration {
+  const {
+    name,
+    projectName,
+    title: titleInConfig,
+    bio,
+    disable,
+    avatar,
+    favicon,
+    twitter: twitterInConfig,
+    list,
+  } = parseYaml(Deno.readTextFileSync(filename)) as ProfileConfiguration;
 
-// TODO: validate with custom schema
-if (!name || !projectName || !avatar || !listInConfig) {
-  throw new Error("missing required data");
-}
+  // TODO: validate with custom schema
+  if (!name || !projectName || !avatar || !list) {
+    throw new Error("missing required data");
+  }
 
-const title = titleInConfig || `${name} profile`;
-const twitter = (twitterInConfig || "").replace(/^([^@])/, "@$1");
-const list = Object.entries(listInConfig);
-if (list.length === 0) {
-  throw new Error("list item is empty");
+  const title = titleInConfig || `${name} profile`;
+  const twitter = (twitterInConfig || "").replace(/^([^@])/, "@$1");
+  if (Object.keys(list).length === 0) {
+    throw new Error("list item is empty");
+  }
+
+  return {
+    name,
+    projectName,
+    title,
+    bio,
+    disable,
+    avatar,
+    favicon,
+    twitter,
+    list,
+  };
 }
 
 // TODO: add validation
@@ -62,80 +73,95 @@ const renderListItem = (listItem: ListItem) => {
 
 const rainCount = 30;
 
-const htmlHead = h(
-  "head",
-  { prefix: "og:http://ogp.me/ns#" },
-  h("meta", { charset: "utf-8" }),
-  h("meta", {
-    name: "viewport",
-    content: "width=device-width,initial-scale=1.0",
-  }),
-  h("meta", { property: "og:url", content: `https://${projectName}.deno.dev` }),
-  h("meta", { property: "og:type", content: "website" }),
-  h("meta", { property: "og:title", content: title }),
-  h("meta", { property: "og:description", content: `About ${name}` }),
-  h("meta", { property: "og:site_name", content: title }),
-  h("meta", { property: "og:image", content: avatar }),
-  h("meta", { name: "twitter:card", content: "summary" }),
-  twitter ? h("meta", { name: "twitter:site", content: twitter }) : "",
-  h("title", title),
-  h("style", getDenoteCss(rainCount)),
-  favicon ? h("link", { rel: "icon", href: favicon }) : "",
-);
+export function renderHtmlHead(config: ProfileConfiguration) {
+  const {
+    name,
+    projectName,
+    title,
+    avatar,
+    favicon,
+    twitter,
+  } = config;
 
-const htmlBody = h(
-  "body",
-  h("div", { class: "rain" }, h("div", { class: "drop" }).repeat(rainCount)),
-  h(
-    "div",
-    { id: "main" },
-    h("img", { alt: "avatar", class: "avatar", src: avatar }),
-    h("h1", name),
-    bio ? h("div", { class: "bio" }, bio) : "",
-    h("div", "Click to jump..."),
-    h(
-      "div",
-      { class: "nav-box" },
-      h(
-        "div",
-        { class: "nav" },
-        ...list.map(([id, listGroup]) =>
-          h("a", { href: `#${id}` }, icongram(listGroup.icon, 26))
-        ),
-      ),
-    ),
-    h(
-      "div",
-      { class: "list-group" },
-      ...list.map(([id, listGroup]) => {
-        const { icon, items } = listGroup;
-        return h("h2", { id }, icongram(icon, 40)) +
-          items.map((listItem) => renderListItem(listItem)).join("");
-      }),
-    ),
-    h(
-      "div",
-      "Powered by ",
-      h("a", {
-        href: "https://deno.com/deploy",
-      }, "Deno Deploy"),
-      " ",
-      exLink,
-    ),
-  ),
-);
-
-const html = "<!DOCTYPE html>" + h("html", htmlHead, htmlBody);
-
-if (Deno.args.includes("--build") || Deno.args.includes("-b")) {
-  Deno.writeTextFileSync("./server.js", createDeployServer(html));
-  Deno.exit(0);
+  return h(
+    "head",
+    { prefix: "og:http://ogp.me/ns#" },
+    h("meta", { charset: "utf-8" }),
+    h("meta", {
+      name: "viewport",
+      content: "width=device-width,initial-scale=1.0",
+    }),
+    h("meta", {
+      property: "og:url",
+      content: `https://${projectName}.deno.dev`,
+    }),
+    h("meta", { property: "og:type", content: "website" }),
+    h("meta", { property: "og:title", content: title! }),
+    h("meta", { property: "og:description", content: `About ${name}` }),
+    h("meta", { property: "og:site_name", content: title! }),
+    h("meta", { property: "og:image", content: avatar }),
+    h("meta", { name: "twitter:card", content: "summary" }),
+    twitter ? h("meta", { name: "twitter:site", content: twitter }) : "",
+    h("title", title),
+    h("style", getDenoteCss(rainCount)),
+    favicon ? h("link", { rel: "icon", href: favicon }) : "",
+  );
 }
 
-const port = 8080;
-const server = serve({ port });
-console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
-for await (const request of server) {
-  const headers = new Headers({ "content-type": "text/html" });
-  request.respond({ status: 200, body: html, headers });
+export function renderHtmlBody(config: ProfileConfiguration) {
+  const {
+    name,
+    bio,
+    avatar,
+    list: listInConfig,
+  } = config;
+  const list = Object.entries(listInConfig);
+
+  return h(
+    "body",
+    h("div", { class: "rain" }, h("div", { class: "drop" }).repeat(rainCount)),
+    h(
+      "div",
+      { id: "main" },
+      h("img", { alt: "avatar", class: "avatar", src: avatar }),
+      h("h1", name),
+      bio ? h("div", { class: "bio" }, bio) : "",
+      h("div", "Click to jump..."),
+      h(
+        "div",
+        { class: "nav-box" },
+        h(
+          "div",
+          { class: "nav" },
+          ...list.map(([id, listGroup]) =>
+            h("a", { href: `#${id}` }, icongram(listGroup.icon, 26))
+          ),
+        ),
+      ),
+      h(
+        "div",
+        { class: "list-group" },
+        ...list.map(([id, listGroup]) => {
+          const { icon, items } = listGroup;
+          return h("h2", { id }, icongram(icon, 40)) +
+            items.map((listItem) => renderListItem(listItem)).join("");
+        }),
+      ),
+      h(
+        "div",
+        "Powered by ",
+        h("a", {
+          href: "https://deno.com/deploy",
+        }, "Deno Deploy"),
+        " ",
+        exLink,
+      ),
+    ),
+  );
+}
+
+export function renderHtml(filename: string) {
+  const config = loadConfig(filename);
+  return "<!DOCTYPE html>" +
+    h("html", renderHtmlHead(config), renderHtmlBody(config));
 }
