d97ac9fb7b202e37da5b554f93c9305cb4a055f8
kawarimidoll <kawarimidoll+git@gmail.com>
2021-08-05 15:28:22 +0900

    feat: add init option

--- denote.ts
+++ denote.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.103.0/flags/mod.ts";
+import { basename, parseCli, stringifyYaml } from "./deps.ts";
 import { build } from "./build.ts";
 import { serve } from "./serve.ts";
 
@@ -20,24 +20,46 @@ Subcommands:
   s, serve <source>  Runs local server without creating any files.
 
 Options:
+  -i, --init         Generates minimal 'denote.yml' file.
   -v, --version      Shows the version number.
   -h, --help         Shows the help message.
 `.trim();
 
 export async function main(cliArgs: string[]) {
-  const { help, version, force, output, port, watch, debug, "_": args } = parse(
+  const {
+    "_": args,
+    debug,
+    force,
+    help,
+    init,
+    output,
+    port,
+    version,
+    watch,
+  } = parseCli(
     cliArgs,
     {
-      boolean: ["help", "version", "force", "watch", "debug"],
-      string: ["output", "port"],
+      boolean: [
+        "debug",
+        "force",
+        "help",
+        "init",
+        "version",
+        "watch",
+      ],
+      string: [
+        "output",
+        "port",
+      ],
       alias: {
-        h: "help",
-        v: "version",
         d: "debug",
         f: "force",
+        h: "help",
+        i: "init",
         o: "output",
-        w: "watch",
         p: "port",
+        v: "version",
+        w: "watch",
       },
       default: {
         port: "8080",
@@ -50,13 +72,61 @@ export async function main(cliArgs: string[]) {
     return 0;
   }
 
+  if (init) {
+    const name = basename(Deno.cwd());
+    const config = stringifyYaml({
+      name,
+      projectName: name,
+      // bio,
+      // avatar: logoURL,
+      // favicon: logoURL,
+    });
+    console.log(config);
+    const outPath = "./denote.yml";
+    try {
+      await Deno.lstat(outPath);
+      if (
+        force || confirm(
+          `The output path ${outPath} already exists. Are you sure to overwrite this file?`,
+        )
+      ) {
+        Deno.writeTextFileSync(outPath, config);
+        console.log(`Denote config file is successfully created: ${outPath}`);
+        return 0;
+      } else {
+        console.warn("Aborting");
+        return 1;
+      }
+    } catch (e) {
+      if (e.name === "NotFound") {
+        Deno.writeTextFileSync(outPath, config);
+        console.log(`Denote config file is successfully created: ${outPath}`);
+        return 0;
+      }
+      console.error(e);
+      return 1;
+    }
+  }
+
   const [subcommand, source] = args;
 
   if (subcommand === "build" || subcommand === "b") {
-    return await build({ help, force, output, source, debug });
+    return await build({
+      debug,
+      force,
+      help,
+      output,
+      source,
+    });
   }
   if (subcommand === "serve" || subcommand === "s") {
-    return await serve({ help, port, source, watch, debug });
+    return await serve({
+      debug,
+      help,
+      port,
+      source,
+      watch,
+    });
   }
   if (help) {
     console.log(helpMsg);
--- deps.ts
+++ deps.ts
@@ -5,10 +5,14 @@ export {
 } from "https://deno.land/std@0.101.0/testing/asserts.ts";
 export { encode } from "https://deno.land/std@0.103.0/encoding/base64.ts";
 export { serve } from "https://deno.land/std@0.103.0/http/server.ts";
-export { parse as parseYaml } from "https://deno.land/std@0.103.0/encoding/yaml.ts";
+export {
+  parse as parseYaml,
+  stringify as stringifyYaml,
+} from "https://deno.land/std@0.103.0/encoding/yaml.ts";
 export { createHash } from "https://deno.land/std@0.103.0/hash/mod.ts";
 export { basename, extname } from "https://deno.land/std@0.103.0/path/mod.ts";
 export { debounce } from "https://deno.land/std@0.103.0/async/mod.ts";
+export { parse as parseCli } from "https://deno.land/std@0.103.0/flags/mod.ts";
 
 export { gzip } from "https://deno.land/x/compress@v0.3.8/gzip/gzip.ts";
 export { tag } from "https://deno.land/x/markup_tag@0.1.2/mod.ts";
