4bdd0cf2a9fd9576ed319252071700da50279fd4
kawarimidoll <kawarimidoll+git@gmail.com>
2021-08-09 20:55:10 +0900

    feat: add http request handlers

--- textdb.ts
+++ textdb.ts
@@ -7,13 +7,64 @@ import {
 
 const TEXTDB = `https://textdb.dev/api/data/${Deno.env.get("TEXTDB_ENDPOINT")}`;
 
+interface DenoteSchema {
+  [name: string]: {
+    hashedToken: string;
+    configPath: string;
+  };
+}
+
 // do not add charaset=utf-8 in headers
+async function getDB(): Promise<DenoteSchema | null> {
+  try {
+    const response = await fetch(
+      TEXTDB,
+      { headers: { "content-type": "application/json" } },
+    );
+
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json);
+      return json;
+    }
+  } catch (error) {
+    console.warn(error);
+  }
+
+  return null;
+}
+
+async function putDB(data: DenoteSchema) {
+  try {
+    const response = await fetch(
+      TEXTDB,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+      },
+    );
+
+    return response.ok;
+  } catch (error) {
+    console.warn(error);
+  }
+
+  return false;
+}
 
 serve({
   "/": async (request) => {
     const { error, body } = await validateRequest(request, {
+      GET: {},
       POST: {
-        body: ["name", "token", "yaml_path"],
+        body: ["name", "token", "config_path"],
+      },
+      PUT: {
+        body: ["name", "token", "config_path"],
+      },
+      DELETE: {
+        body: ["name", "token"],
       },
     });
     if (error) {
@@ -23,43 +74,82 @@ serve({
       return json({ message: "something went wrong" }, { status: 500 });
     }
 
-    // console.log(JSON.stringify(body));
-    if (request.method === "POST") {
-      const response = await fetch(
-        TEXTDB,
-        {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          body: JSON.stringify(body),
-        },
-      );
+    console.log(JSON.stringify(body));
 
-      if (response.ok) {
+    const db = await getDB();
+    const name = `${body.name}`;
+    const hashedToken = `###${body.token}###`;
+    const configPath = `${body.config_path}`;
+
+    if (!db) {
+      return json({ message: "failed to access database" }, { status: 500 });
+    }
+
+    if (request.method === "GET") {
+      return json({ message: "current db", db });
+    }
+
+    if (request.method === "POST") {
+      if (db[name]) {
         return json({
-          message: "data saved!",
-          name: body.name,
-          yaml_path: body["yaml_path"],
+          message:
+            "the name is already exist. if you update the data, please use PUT request.",
+        }, { status: 400 });
+      }
+
+      db[name] = { hashedToken, configPath };
+
+      if (putDB(db)) {
+        return json({
+          message: "data saved! do not forget your token",
+          ...body,
         });
-      } else {
-        return json({ message: "something went wrong" }, { status: 500 });
       }
     }
 
-    return new Response("hello world");
+    if (request.method === "PUT") {
+      if (!db[name]) {
+        return json({
+          message:
+            "the name is not exist. if you add the data, please use POST request.",
+        }, { status: 400 });
+      }
+
+      db[name] = { hashedToken, configPath };
+
+      if (putDB(db)) {
+        return json({
+          message: "data updated!",
+          config_path: configPath,
+        });
+      }
+    }
+
+    if (request.method === "DELETE") {
+      if (!db[name]) {
+        return json({
+          message: "the name is not exist.",
+        }, { status: 400 });
+      }
+
+      delete db[name];
+
+      if (putDB(db)) {
+        return json({
+          message: "data deleted!",
+        });
+      }
+    }
+    return json({ message: "something went wrong" }, { status: 500 });
   },
   "/:slug": async (_: Request, params: PathParams) => {
     const name = Array.isArray(params.slug) ? params.slug[0] : params.slug;
     console.log(name);
-    const response = await fetch(
-      TEXTDB,
-      {
-        headers: { "content-type": "application/json" },
-      },
-    );
 
-    if (response.ok) {
-      const db = await response.json();
-      const data = JSON.parse(db)[name];
+    const db = await getDB();
+
+    if (db) {
+      const data = db[name];
 
       if (data) {
         return json({ data });
