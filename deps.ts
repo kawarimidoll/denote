0fd6c2ad0ac733b55d836c76d024a9c90aa20a95
kawarimidoll <kawarimidoll+git@gmail.com>
2022-02-08 21:06:32 +0900

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -2,42 +2,42 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.103.0/testing/asserts.ts";
+} from "https://deno.land/std@0.125.0/testing/asserts.ts";
 export {
   decode,
   encode,
-} from "https://deno.land/std@0.103.0/encoding/base64.ts";
+} from "https://deno.land/std@0.125.0/encoding/base64.ts";
 export {
   parse as parseYaml,
   stringify as stringifyYaml,
-} from "https://deno.land/std@0.103.0/encoding/yaml.ts";
-export { createHash } from "https://deno.land/std@0.103.0/hash/mod.ts";
-export { basename, extname } from "https://deno.land/std@0.103.0/path/mod.ts";
-export { debounce } from "https://deno.land/std@0.103.0/async/mod.ts";
-export { parse as parseCli } from "https://deno.land/std@0.103.0/flags/mod.ts";
-export { mapValues } from "https://deno.land/std@0.103.0/collections/mod.ts";
+} from "https://deno.land/std@0.125.0/encoding/yaml.ts";
+export { createHash } from "https://deno.land/std@0.125.0/hash/mod.ts";
+export { basename, extname } from "https://deno.land/std@0.125.0/path/mod.ts";
+export { debounce } from "https://deno.land/std@0.125.0/async/mod.ts";
+export { parse as parseCli } from "https://deno.land/std@0.125.0/flags/mod.ts";
+export { mapValues } from "https://deno.land/std@0.125.0/collections/mod.ts";
 
 export {
   json,
   serve as sift,
   validateRequest,
-} from "https://deno.land/x/sift@0.3.5/mod.ts";
+} from "https://deno.land/x/sift@0.4.3/mod.ts";
 export { lt as semverLessThan } from "https://deno.land/x/semver@v1.4.0/mod.ts";
-export { gunzip, gzip } from "https://deno.land/x/compress@v0.3.8/gzip/gzip.ts";
+export { gunzip, gzip } from "https://deno.land/x/compress@v0.4.1/gzip/gzip.ts";
 export {
   AMP,
   GT,
   LT,
   QUOT,
   tag,
-} from "https://deno.land/x/markup_tag@0.1.2/mod.ts";
-import shuffle from "https://deno.land/x/shuffle@v1.0.0/mod.ts";
+} from "https://deno.land/x/markup_tag@0.3.0/mod.ts";
+import shuffle from "https://deno.land/x/shuffle@v1.0.1/mod.ts";
 export { shuffle };
-export { range } from "https://deno.land/x/it_range@v1.0.2/mod.ts";
+export { range } from "https://deno.land/x/it_range@v1.1.0/mod.ts";
 
 export {
   DeleteItemCommand,
   DynamoDBClient,
   GetItemCommand,
   PutItemCommand,
-} from "https://cdn.skypack.dev/@aws-sdk/client-dynamodb@3.22.0?dts#=";
+} from "https://cdn.skypack.dev/@aws-sdk/client-dynamodb?dts";
